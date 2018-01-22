eb8d8bad0f3d8e92aeee763a4545127ee99a6666
Mario Junior <juniorm3@msn.com>
2018-01-21 22:53:10 -0200

    Configuração para aprimeira tela de cadastro

---
+++ src/main/java/com/jrm3/controller/GestaoClientesBean.java
@@ -0,0 +1,14 @@
+package com.jrm3.controller;
+
+import java.io.Serializable;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class GestaoClientesBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+}
